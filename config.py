ba897c70d4d7a928ec5954f6d798262a9053f4e7
Pietro Ciani <pietro.ciani@stud.unifi.it>
2023-06-12 10:05:52 +0200

    database link via database_url env

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
-import os
+import dj_database_url
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
